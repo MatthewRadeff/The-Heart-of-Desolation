829e37b3c8730ff9a341d927fde39306c6c026c1
MatthewRadeff <matt_radeff@hotmail.com>
2017-09-17 16:41:45 -0400

    Scenes are set up
    the main menu, how to play, and credits are all setup. Now its time to
    work on the game itself. Most of the objects that are supposed to spawn
    are ready, still need a main character, and still wondering on how the
    game world should look.

---
+++ The Heart of Desolation/Assets/MyAssets/Scripts/Misc/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// code by: Matthew C. Radeff
+
+public class ButtonScript : MonoBehaviour
+{
+
+    // passes a string into load scene, which goes to that specific scene
+    public void ChangeScene(string m_sceneName)
+    {
+        SceneManager.LoadScene(m_sceneName);
+    }
+
+
+    // shuts the application down
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+
+}
