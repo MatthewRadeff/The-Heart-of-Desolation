e7eb824e966c8d672380dbe9eb1f3a545712aa0f
MatthewRadeff <matt_radeff@hotmail.com>
2018-01-25 15:31:21 -0500

    randomizing platforms with object pooling
    the platforms are being ranzomized and handled now with object pooling. The platforms also spawn at the end of the previous one so that they dont overlap.

--- The Heart of Desolation/Assets/MyAssets/Scripts/Level/PlatformGenerator.cs
+++ The Heart of Desolation/Assets/MyAssets/Scripts/Level/PlatformGenerator.cs
@@ -23,7 +23,7 @@ public class PlatformGenerator : MonoBehaviour
     public float m_distanceBetweenMax;
 
     // array of platforms
-    public GameObject[] m_thePlatforms;
+    //public GameObject[] m_thePlatforms;
     // which platform to use in array
     private int m_platformSelector;
 
@@ -31,8 +31,11 @@ public class PlatformGenerator : MonoBehaviour
     private float[] m_platformWidths;
 
 
+
+
+
     // referencing the object pooler script
-    //public ObjectPooler m_theObjectPool;
+    public ObjectPooler[] m_theObjectPools;
 
 
 	// Use this for initialization
@@ -41,11 +44,12 @@ public class PlatformGenerator : MonoBehaviour
         // finds how wide the platform is getting the box collider size
         //m_platformWidth = m_thePlatform.GetComponent<BoxCollider2D>().size.x;
 
-        m_platformWidths = new float[m_thePlatforms.Length];
+        m_platformWidths = new float[m_theObjectPools.Length];
 
-        for(int i = 0; i < m_thePlatforms.Length; i++)
+        for(int i = 0; i < m_theObjectPools.Length; i++)
         {
-            m_platformWidths[i] = m_thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+            // getting width of the pooled object
+            m_platformWidths[i] = m_theObjectPools[i].m_pooledObject.GetComponent<BoxCollider2D>().size.x;
 
         }
 
@@ -60,24 +64,28 @@ public class PlatformGenerator : MonoBehaviour
             m_distanceBetween = Random.Range(m_distanceBetweenMin,m_distanceBetweenMax);
 
             // selecting a random platform in array
-            m_platformSelector = Random.Range(0, m_thePlatforms.Length);
+            m_platformSelector = Random.Range(0, m_theObjectPools.Length);
 
             // moving platform spawn location a certain distance from the last position taking in all the distances 
-            transform.position = new Vector3(transform.position.x + m_platformWidths[m_platformSelector] + m_distanceBetween, transform.position.y, transform.position.z);
+            // dividing by 2 makes the spawn location occur at the end of the platform, so at least no overlapping happens
+            transform.position = new Vector3(transform.position.x + (m_platformWidths[m_platformSelector] / 2) + m_distanceBetween, transform.position.y, transform.position.z);
 
 
             // creating the platform
-            Instantiate(/*m_thePlatform*/m_thePlatforms[m_platformSelector], transform.position, transform.rotation);
+            //Instantiate(/*m_thePlatform*/m_theObjectPools[m_platformSelector], transform.position, transform.rotation);
             //Debug.Log("Long platform created!");
 
             // creating "new platform" from using the pooling
             // setting position rotation 
-           /* GameObject m_newPlatform = m_theObjectPool.GetPooledObject();
+            GameObject m_newPlatform = m_theObjectPools[m_platformSelector].GetPooledObject();
             m_newPlatform.transform.position = transform.position;
             m_newPlatform.transform.rotation = transform.rotation;
             // also setting it to true because up until now it has been not active
             m_newPlatform.SetActive(true);
-            */
+
+            // adding on half the distance from where the last platform was so the platforms dont overlap
+            transform.position = new Vector3(transform.position.x + (m_platformWidths[m_platformSelector] / 2), transform.position.y, transform.position.z);
+
 
         }
 
