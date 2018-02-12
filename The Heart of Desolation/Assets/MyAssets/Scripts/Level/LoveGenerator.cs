4cca446b8952772fc13c917d4f4287da665deffe
MatthewRadeff <matt_radeff@hotmail.com>
2018-02-11 21:15:46 -0500

    love and memories
    love and memory pickups are spawning and working. The final thing that needs to be added in is the sound/music.

--- The Heart of Desolation/Assets/MyAssets/Scripts/Level/LoveGenerator.cs
+++ The Heart of Desolation/Assets/MyAssets/Scripts/Level/LoveGenerator.cs
@@ -6,6 +6,7 @@ public class LoveGenerator : MonoBehaviour
 {
 
     public ObjectPooler m_lovePool;
+    
 
     // distance between each pickup
     public float m_distanceBetweenPickups;
@@ -13,22 +14,21 @@ public class LoveGenerator : MonoBehaviour
     // where pickups will be generated from 
     public void SpawnLovePickups(Vector3 startPosition)
     {
-        // generating the pickup in the middle
+        /*// generating the pickup in the middle
         GameObject lovePickup = m_lovePool.GetPooledObject();
         lovePickup.transform.position = startPosition;
         lovePickup.SetActive(true);
+        */
         // generating the pickup on the left
         GameObject lovePickup2 = m_lovePool.GetPooledObject();
         lovePickup2.transform.position = new Vector3(startPosition.x - m_distanceBetweenPickups, startPosition.y, startPosition.z);
         lovePickup2.SetActive(true);
-        // generating the pickup on the right
+       // generating the pickup on the right
         GameObject lovePickup3 = m_lovePool.GetPooledObject();
         lovePickup3.transform.position = new Vector3(startPosition.x + m_distanceBetweenPickups, startPosition.y, startPosition.z);
         lovePickup3.SetActive(true);
-
+        
 
     }
-
-
 
 }
